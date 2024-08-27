ad71fbe8cbd7229e5761b994d5b72ad6150e7cf8
Lucas Julian Guerci <lucasjulian.guerci@estudiantes.unahur.edu.ar>
2024-08-27 13:50:19 -0300

    linted

--- src/utils/__test__/promise.spec.ts
+++ src/utils/__test__/promise.spec.ts
@@ -1,45 +1,45 @@
-import handlePromise, { handlePromiseWithId }  from '../promise';
+import handlePromise, { handlePromiseWithId } from '../promise';
 
 describe('UTILS > PROMISE', () => {
-    describe('handlePromise', () => {
-        it('should return data when promise resolves', async () => {
-            const resolvedValue = 'resolved data';
-            const promise = Promise.resolve(resolvedValue);
+  describe('handlePromise', () => {
+    it('should return data when promise resolves', async () => {
+      const resolvedValue = 'resolved data';
+      const promise = Promise.resolve(resolvedValue);
 
-            const result = await handlePromise(promise);
+      const result = await handlePromise(promise);
 
-            expect(result).toStrictEqual([resolvedValue]);
-        });
-
-        it('should return null and error when promise rejects', async () => {
-            const errorValue = new Error('rejected error');
-            const promise = Promise.reject(errorValue);
-
-            const result = await handlePromise(promise);
-
-            expect(result).toStrictEqual([null, errorValue]);
-        });
+      expect(result).toStrictEqual([resolvedValue]);
     });
 
-    describe('handlePromiseWithId', () => {
-        it('should return id and data when promise resolves', async () => {
-            const id = '123';
-            const resolvedValue = 'resolved data';
-            const promise = Promise.resolve(resolvedValue);
+    it('should return null and error when promise rejects', async () => {
+      const errorValue = new Error('rejected error');
+      const promise = Promise.reject(errorValue);
 
-            const result = await handlePromiseWithId(id, promise);
+      const result = await handlePromise(promise);
 
-            expect(result).toStrictEqual({ id, data: resolvedValue });
-        });
-
-        it('should return id and error when promise rejects', async () => {
-            const id = '123';
-            const errorValue = new Error('rejected error');
-            const promise = Promise.reject(errorValue);
-
-            const result = await handlePromiseWithId(id, promise);
-
-            expect(result).toStrictEqual({ id, error: errorValue });
-        });
+      expect(result).toStrictEqual([null, errorValue]);
     });
+  });
+
+  describe('handlePromiseWithId', () => {
+    it('should return id and data when promise resolves', async () => {
+      const id = '123';
+      const resolvedValue = 'resolved data';
+      const promise = Promise.resolve(resolvedValue);
+
+      const result = await handlePromiseWithId(id, promise);
+
+      expect(result).toStrictEqual({ id, data: resolvedValue });
+    });
+
+    it('should return id and error when promise rejects', async () => {
+      const id = '123';
+      const errorValue = new Error('rejected error');
+      const promise = Promise.reject(errorValue);
+
+      const result = await handlePromiseWithId(id, promise);
+
+      expect(result).toStrictEqual({ id, error: errorValue });
+    });
+  });
 });
--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,7 +1,9 @@
-export default function handlePromise(promise:Promise<any>) {
-    return promise.then((data) => [data]).catch((err) => [null, err]);
+export default function handlePromise(promise: Promise<any>) {
+  return promise.then((data) => [data]).catch((err) => [null, err]);
 }
 
-export function handlePromiseWithId(id,promise:Promise<any>) {
-    return promise.then((data) => ({id,data})).catch((error) => ({id,error}));
+export function handlePromiseWithId(id, promise: Promise<any>) {
+  return promise
+    .then((data) => ({ id, data }))
+    .catch((error) => ({ id, error }));
 }
