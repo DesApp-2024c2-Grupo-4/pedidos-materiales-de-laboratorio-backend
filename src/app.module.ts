8bc477c6b7b62e825ac924f533f37faa930893fd
Lucas Julian Guerci <lucasjulian.guerci@estudiantes.unahur.edu.ar>
2024-11-03 18:10:29 -0300

    feat: added default user creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 /* Providers */
 import MongooseProvider from './config/mongoose.provider';
 import { AuthGuardProvider } from './auth/providers/auth-guard.provider';
-import  { EquipmentModule } from './equipment/equipment.module';
+import { EquipmentModule } from './equipment/equipment.module';
 import { EquipmentController } from './equipment/equipment.controller';
 
 /* App Modules */
@@ -13,11 +13,13 @@ import { AuthModule } from './auth/auth.module';
 import { ConversationModule } from './conversation/conversation.module';
 import { MaterialModule } from './material/material.module';
 import { ReactiveModule } from './reactive/reactive.module';
+import { UserInitModule } from './user/user-init/user-init.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync(MongooseProvider),
+    UserInitModule,
     AuthModule,
     EquipmentModule,
     ConversationModule,
---
+++ src/user/user-init/user-init.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../../schemas/user.schema';
+import { ConfigService } from '@nestjs/config';
+import handlePromise from 'src/utils/promise';
+
+@Injectable()
+export class UserInitService implements OnModuleInit {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private configService: ConfigService,
+  ) {}
+
+  async onModuleInit() {
+    if (
+      this.configService
+        .get<string>('CREATE_DEFAULT_ADMIN')
+        ?.toLocaleLowerCase() !== 'true'
+    ) {
+      return;
+    }
+
+    const userCount = await this.userModel.countDocuments().exec();
+
+    if (userCount === 0) {
+      console.warn('No user accounts detected, creating default admin user.');
+
+      const envRoles = this.configService.get<string>('DEFAULT_ADMIN_ROLE');
+      const role: string[] = envRoles ? envRoles.split(';') : ['admin', 'lab'];
+      const email =
+        this.configService.get<string>('DEFAULT_ADMIN_EMAIL') ||
+        'admin@example.com';
+      const password =
+        this.configService.get<string>('DEFAULT_ADMIN_PASSWORD') || 'example';
+      const name =
+        this.configService.get<string>('DEFAULT_ADMIN_NAME') || 'Admin';
+      const lastName =
+        this.configService.get<string>('DEFAULT_ADMIN_LASTNAME') || 'User';
+      const dni = parseInt(
+        this.configService.get<string>('DEFAULT_ADMIN_DNI') || '12345678',
+        10,
+      );
+
+      const [admin, err] = await handlePromise(
+        this.userModel.create({
+          email,
+          password,
+          name,
+          lastName,
+          dni,
+          role,
+        }),
+      );
+
+      if (err) {
+        throw new Error(`Cannot create admin user ${admin}. Reason: ${err}`);
+      }
+
+      console.warn(`Default admin user ${email} created.`);
+    }
+  }
+}
